67d656d83cced99ab455b43d0ede783af38abc5b
Nguyễn Trí Minh <minh.ngntri@gmail.com>
2023-03-19 22:23:37 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,34 +1,70 @@
-// Select randomly an option
-function selectRandomOption(question) {
-const options = question.querySelectorAll('div[role="radio"]');
-const randomIndex = Math.floor(Math.random() * options.length);
-options[randomIndex].click();
-}
-
-// Auto-filling process
-setTimeout(() => {
-    // Check if the current page is the completion page, then submit a new form
-    const aElements = document.querySelectorAll('a');
-    aElements.forEach((aElement) => {
-        if (aElement.getAttribute('href').includes('viewform?usp=form_confirm')) {
-            aElement.click();
+// Check final page to reload the form
+function reloadForm() {
+    let isSubmissionPage = false;
+    const buttonElements = document.querySelectorAll('div');
+    buttonElements.forEach((buttonElement) => {
+        if (buttonElement.getAttribute('jsname') === "M2UYVd") {
+            isSubmissionPage = true;
         }
     });
+    if (window.location.href.indexOf("formResponse") > -1 && isSubmissionPage === false) {
+        window.location.reload();
+    }  
+}
 
-    // Get all radio button questions on the page
+// Fill all email and text fields
+function fillTextFields() {
+    email = 'anemail@is.here';
+    text = 'A text is here';
+    const event = new Event('input', { bubbles: true });
+    const textElements = document.querySelectorAll('input');
+    textElements.forEach((textElement) => {
+        if (textElement.getAttribute('type') === "email") {
+            textElement.value = email;
+            textElement.dispatchEvent(event);
+        }
+        else if (textElement.getAttribute('type') === "text") {
+            textElement.value = text;
+            textElement.dispatchEvent(event);
+        }
+    });
+}
+
+// Select randomly an option
+function selectRandomOption(question) {
+    const options = question.querySelectorAll('div[role="radio"]');
+    const randomIndex = Math.floor(Math.random() * options.length);
+    options[randomIndex].click();
+}
+
+// Fill all multiple choice questions
+function fillRadioButtonFields() {
     const radioQuestions = document.querySelectorAll('div[role="radiogroup"]');
     
     radioQuestions.forEach(question => {
         selectRandomOption(question);
-    });
+    });    
+}
+
+// Navigate to the next page or submit the form
+function navigateOrSubmit() {
+    const buttonElements = document.querySelectorAll('div');
+    buttonElements.forEach((buttonElement) => {
+        if (buttonElement.getAttribute('jsname') === "OCpkoe" || buttonElement.getAttribute('jsname') === "M2UYVd") {
+            buttonElement.click();
+        }
+    });  
+}
+
+// Auto-filling process
+setTimeout(() => {
+    reloadForm();
+
+    fillTextFields();
+
+    fillRadioButtonFields();
 
     setTimeout(() => {
-        // Move to the next page or submit the form
-        const divElements = document.querySelectorAll('div');
-        divElements.forEach((divElement) => {
-            if (divElement.getAttribute('jsname') === "OCpkoe" || divElement.getAttribute('jsname') === "M2UYVd") {
-                divElement.click();
-            }
-        });        
+        navigateOrSubmit();    
     }, 2000);
 }, 2000);
