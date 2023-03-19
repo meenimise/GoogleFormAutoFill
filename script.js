56bde4b1107c15be619e049669f1c3407d23a7f9
Nguyễn Trí Minh <minh.ngntri@gmail.com>
2023-03-19 17:52:06 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,39 @@
+// Select randomly an option
+function selectRandomOption(question) {
+const options = question.querySelectorAll('div[role="radio"]');
+const randomIndex = Math.floor(Math.random() * options.length);
+options[randomIndex].click();
+}
+
+// Auto-filling process
+setTimeout(() => {
+    // Check if the current page is the completion page, then submit a new form
+    const aElements = document.querySelectorAll('a');
+    aElements.forEach((aElement) => {
+        if (aElement.textContent === 'Submit another response') {
+            aElement.click();
+        }
+    });
+
+    // Get all radio button questions on the page
+    const radioQuestions = document.querySelectorAll('div[role="radiogroup"]');
+    
+    radioQuestions.forEach(question => {
+        selectRandomOption(question);
+    });
+
+    setTimeout(() => {
+        // Move to the next page
+        const spanElements = document.querySelectorAll('span');
+        spanElements.forEach((spanElement) => {
+            if (spanElement.textContent === 'Next') {
+                const divElement = spanElement.parentElement;
+                divElement.click();
+            }
+            else if (spanElement.textContent === 'Submit') {
+                const divElement = spanElement.parentElement;
+                divElement.click();
+            }
+        });		
+    }, 2000);
+}, 2000);
